5f1e2c9038b93b38f623ea9ac6e075c00ad18e9a
RodrigolCasimiro <rodrigolcasimiro@gmail.com>
2024-02-22 14:29:06 +0000

    Erro Arduino

--- sketch_feb17a/ard1.ino
+++ sketch_feb17a/ard1.ino
@@ -1,4 +1,5 @@
 const int AnalogIn = A0;
+const int ledPin = 13; // LED pin
 
 int comando;
 int variable;
@@ -8,9 +9,22 @@ void setup() {
 	Serial.setTimeout(.1);
 }
 void loop() {
-
+  if (comando != "1") {
+    handleError();
+  }
   variable = analogRead(AnalogIn);
 	while (!Serial.available());
 	comando = Serial.readString().toInt();
 	if (comando == 1)Serial.print(variable*5./1024.);
+}
+
+void handleError(str errorMessage) {
+  Serial.println(errorMessage);
+  while(1) { // Start an infinite loop
+    digitalWrite(ledPin, HIGH); // Turn the LED on
+    delay(250);                 // Wait for 250 milliseconds
+    digitalWrite(ledPin, LOW);  // Turn the LED off
+    delay(250);                 // Wait for 250 milliseconds
+  }
+  // No code here will ever execute because of the while(1) loop.
 }
