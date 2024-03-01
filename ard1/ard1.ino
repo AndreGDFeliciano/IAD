fd7ac5bebcc7ab36e691cd3f8b83cfcdf8b11d44
RodrigolCasimiro <rodrigolcasimiro@gmail.com>
2024-03-01 12:24:18 +0000

    comentarios ard.ino

--- ard1/ard1.ino
+++ ard1/ard1.ino
@@ -1,9 +1,12 @@
+// Sets up pins
 const int AnalogIn = A0;
 const int ledPin = 13;
 
+// Declare variables
 int comando;
 int variable;
 
+// Serial and pin setup
 void setup() {
 	Serial.begin(9600);
 	Serial.setTimeout(.1);
@@ -11,14 +14,14 @@ void setup() {
 	pinMode(AnalogIn, INPUT);
 }
 void loop() {
-  while (!Serial.available());
+	while (!Serial.available()); // Waits for a command from the RaspberryPi
 	comando = Serial.readString().toInt();
 	if (comando == 1) {
-    variable = analogRead(AnalogIn);
-		Serial.println(variable*5./1024.);
-    digitalWrite(ledPin,LOW);
+    	variable = analogRead(AnalogIn);
+		Serial.println(variable*5./1024.); // Sends input value to RaspberryPi
+    	digitalWrite(ledPin,LOW);
 	} else {
-    Serial.println("Error: Invalid Command"); // Envia mensagem de erro
-    digitalWrite(ledPin,HIGH);
+    	Serial.println("Error: Invalid Command"); // Sends error to RaspberryPi
+    	digitalWrite(ledPin,HIGH); // Lights up the LED when error is found
   }
 }
