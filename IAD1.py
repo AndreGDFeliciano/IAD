beebc7091ef1260facb6a0aed4fde7fa6fb77dff
IAD - André e Rodrigo <andregdfeliciano@gmail.com>
2024-03-01 12:46:12 +0000

    Final

--- IAD1.py
+++ IAD1.py
@@ -11,11 +11,10 @@ __author__ = "ist1100286 André Feliciano & ist1103132 Rodrigo Casimiro"
 __version__ = "42"
 
 
-import PyQt5
+
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,\
                             QMessageBox,QVBoxLayout,QInputDialog
-import pyqtgraph as pg
 from pyqtgraph import PlotWidget
 import os
 import serial
@@ -32,9 +31,12 @@ class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
+        # Create lists to store values to graph
         self.xval = []
         self.yval = []
+        # Create time count
         self.t    = 0
+        #Create a place to store command and time interval, set the default
         self.command  = "1"
         self.time_int = 1000
         self.initUI()
@@ -42,8 +44,18 @@ class MyApp(QWidget):
     def initUI(self):
         """
         Starts the UI for the windows program
+        2 buttons:
+            Send 1 Command
+            Start/Stop periodic command
+        1 Graph:
+            yaxis: Voltage read in arduino
+            xaxis: time
+        3 buttons
+            Change comand
+            Change time interval
+            Clear graph
         """
-        #self.setGeometry(300, 300, 500, 400) # (x, y, width, height)
+        self.showMaximized() #maximise window at start
         self.setWindowTitle('First IAD Project')
 
         # Creating Buttons with tooltips
@@ -54,12 +66,12 @@ class MyApp(QWidget):
 
         self.buttonStart = QPushButton('Start/Stop periodic command', self)
         self.buttonStart.setToolTip(
-                    'Sends commands periodically and updates graph')
+            'Sends commands periodically and updates graph while toggled')
         self.buttonStart.setCheckable(True) # make it a toggle button
         self.buttonStart.setChecked(False)  # Guarantee it starts turned off
         self.buttonStart.toggled.connect(self.toggleGraphUpdate)
 
-        self.buttonChangeC = QPushButton('Change Command', self)
+        self.buttonChangeC = QPushButton('Change command', self)
         self.buttonChangeC.setToolTip('Click to change command')
         self.buttonChangeC.clicked.connect(self.inputCommand)
 
@@ -67,16 +79,15 @@ class MyApp(QWidget):
         self.buttonChangeT.setToolTip('Click to change time interval')
         self.buttonChangeT.clicked.connect(self.inputTime)
 
-        self.buttonCleanGraph = QPushButton('Clear Graph', self)
-        self.buttonCleanGraph.setToolTip('Click to clear the graph below')
-        self.buttonCleanGraph.clicked.connect(self.cleanGraph)
+        self.buttonClearGraph = QPushButton('Clear graph', self)
+        self.buttonClearGraph.setToolTip('Click to clear the graph below')
+        self.buttonClearGraph.clicked.connect(self.clearGraph)
 
 
         # Create layout
         layout = QVBoxLayout()
 
         #Add buttons on top of the graphic
-        layout.addWidget(self.buttonTest)
         layout.addWidget(self.buttonCommand)
         layout.addWidget(self.buttonStart)
 
@@ -94,7 +105,7 @@ class MyApp(QWidget):
         # Add the buttons below the graph
         layout.addWidget(self.buttonChangeC)
         layout.addWidget(self.buttonChangeT)
-        layout.addWidget(self.buttonCleanGraph)
+        layout.addWidget(self.buttonClearGraph)
 
         self.setLayout(layout)
 
@@ -105,30 +116,30 @@ class MyApp(QWidget):
 
     def sendCommand(self):
         """
-        Creates a Message Box and sends a command to the arduino.
-        Reads the Voltage in the arduino
+        Creates a QMessageBox and sends a command to the arduino.
+        Reads the voltage in the arduino
         """
         QMessageBox.information(self, 'Command sent',
                 f'Voltage measured:{self.comando()}')
 
     def comando(self):
         """
-        Sends command to arduino
+        Sends command to arduino..
         Returns:
             float: measured value
-        Error in case there is a wrong command
+        Error in case there is a wrong command or empty string
         """
         arduino.write(bytes(self.command,"utf-8"))
         time.sleep(0.001) # wait for the arduino to write to the Serial in s
         read = arduino.readline().decode("UTF-8").rstrip()
         # rstring takes out the \n and \r at the end of the line
-        if read == "Error: Invalid Command":
+        if read == "Error: Invalid command":
             QMessageBox.information(self, 'Invalid Command',
             f'Function called with command: {self.command} \n \
 Correct command ("1") was reset')
             self.command = "1"
             raise ValueError("Error: Invalid Command")
-        if read == "": # in order to avoid the unknown error of empty string
+        elif read == "": # in order to avoid the unknown error of empty string
             raise ValueError("Empty string")
         else:
             read = float(read)
@@ -137,9 +148,7 @@ Correct command ("1") was reset')
 
     def toggleGraphUpdate(self):
         """
-        Sets the timer for updating graph at a set interval
-        Receives:
-            time = 1000 (int, optional): time interval
+        Sets the timer for updating graph at the set interval.
         """
         if self.buttonStart.isChecked():
             self.new = True
@@ -151,9 +160,9 @@ Correct command ("1") was reset')
 
     def updateGraph(self):
         """
-        Define the action to take at timeout update graph
-        Receives:
-            time = 1000 (int, optional): time interval
+        Generate new data
+        Print value to terminal
+        Update graph
         """
         # Generate new data for the plot
         value = self.comando()
@@ -166,8 +175,8 @@ Correct command ("1") was reset')
 
     def inputCommand(self):
         """
-        Changes the command to send in the comando function.
-        Opens a window and asks for the user to write the new command.
+        Opens a window and asks for the user to choose the new command
+        Tests if it is the right command
         """
         comm, ok = QInputDialog.getInt(self, 'Input command',
                                     'Enter new command \nCorrect command: 1')
@@ -181,8 +190,8 @@ Correct command ("1") was reset')
 
     def inputTime(self):
         """
-        Changes the time interval for sending commands and updating graph.
-        Opens a window and asks for the user to write the new time interval.
+        Opens a window and asks for the user to choose the new time interval.
+        In case the toggle is checked: clears the graph and keeps adding values
         """
         time, ok = QInputDialog.getInt(self, 'Input time interval',
                                         'Enter new time interval (ms):',
@@ -191,8 +200,11 @@ Correct command ("1") was reset')
             QMessageBox.information(self, 'Input Values',
                                     f'You entered: {time}')
             self.time_int = time
+        if self.buttonStart.isChecked():
+            self.clearGraph()
+            self.toggleGraphUpdate()
 
-    def cleanGraph(self):
+    def clearGraph(self):
         """
         Clears the graph
         """
@@ -201,6 +213,7 @@ Correct command ("1") was reset')
         self.yval = []
         self.t    = 0
 
+# Starts the new window
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
